68d8d4dfc5c9e6df3e8e9976d4857ceb81148249
Andrea Soares <asoaresdev@gmail.com>
2023-05-23 16:39:39 -0300

    feat: versão 1 do projeto

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,34 @@
  * 
  * 
  */
+
+console.log("Boas vindas ao jogo de BlackJack!");
+const decision = confirm("Quer iniciar uma nova rodada?")
+let carta1
+let carta2
+let carta1Computador
+let carta2Computador
+
+
+if (decision) {
+   carta1 = comprarCarta()
+   carta2 = comprarCarta()
+   carta1Computador = comprarCarta()
+   carta2Computador = comprarCarta()
+   const pontuação = carta1.valor + carta2.valor
+   const pontuaçãoComputador = carta1Computador.valor + carta2Computador.valor
+
+   console.log(`Usuário - cartas: ${carta1.texto} ${carta2.texto} - pontuação ${pontuação} ${"\n"}Computador - cartas: ${carta1Computador.texto} ${carta2Computador.texto} - pontuação ${pontuaçãoComputador}
+   `);
+   if (pontuação === pontuaçãoComputador) {
+      console.log("Empate");
+   } else if (pontuação > pontuaçãoComputador) {
+      console.log("O usuário ganhou!");
+   } else if (pontuação < pontuaçãoComputador) {
+      console.log("O computador ganhou!");
+   }
+
+} else {
+   console.log("O jogo acabou");
+}
+
